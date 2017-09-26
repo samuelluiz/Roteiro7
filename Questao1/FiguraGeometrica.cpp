c6a6eaaadf05ab41ac999a03972a7c4546739d27
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2017-09-26 11:35:02 -0300

    Create FiguraGeometrica.cpp

---
+++ Questao1/FiguraGeometrica.cpp
@@ -0,0 +1,15 @@
+#include "FiguraGeometrica.h"
+#include <string>
+
+using namespace std;
+FiguraGeometrica::FiguraGeometrica(string n)
+{
+    nome = n;
+}
+
+FiguraGeometrica::~FiguraGeometrica()
+{
+
+}
+
+float FiguraGeometrica::calcularArea(){}
